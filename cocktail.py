ffeba3b86d5046454448be0213507d2f11fecbbe
MaximeFouche <114243800+MaximeFouche@users.noreply.github.com>
2023-10-10 17:39:31 +0200

    Add files via upload

--- cocktail.py
+++ cocktail.py
@@ -4,12 +4,13 @@ import asyncio
 import sys,time, datetime
 import numpy as np
 import logging
+import threading
 
 t0=time.time()
 logging.basicConfig(level=logging.DEBUG,
                     filename="cocktail.log",
                     filemode="a",
-                    format='%(asctime)s - %(levelname)s - %()s')
+                    format='%(asctime)s - %(levelname)s - %(message)s')
 
 class Accessoire():
     def __init__(self):
@@ -19,25 +20,25 @@ class Pic(Accessoire):
     """ Un pic peut embrocher un post-it par-dessus les post-it déjà présents
         et libérer le dernier embroché. """
     def embrocher(self,postit):
+        print(type(self.contenu))
         self.contenu.append(postit)
         if verbo!=0:
-            print(f"[{self.__class__.__name__}] ({time.time()-t0}) post-it '{postit}' embroché")
+            print(f"[{self.__class__.__name__}] post-it '{postit}' embroché")
             if verbo==2:
-                print(f"[{self.__class__.__name__}] ({time.time()-t0}) état={self.contenu}")
+                print(f"[{self.__class__.__name__}] état={self.contenu}")
         logging.info(f"Le post-it {postit} fut embroché au bout de {time.time()-t0} secondes")
         return self.contenu
     def liberer(self):
         if len(self.contenu)==0:
-            if verbo ==2: print(f"[{self.__class__.__name__}] ({time.time()-t0}) état={self.contenu}")
-            print(f"({time.time()-t0}) Pic est vide")
+            if verbo ==2: print(f"[{self.__class__.__name__}] état={self.contenu}")
+            print(f"Pic est vide")
             logging.info(f"Le pic est vide au bout de {time.time()-t0} secondes")
         else:
-            postit=self.contenu[-1]
             if verbo!=0:
                 if verbo==2:
-                    print(f"[{self.__class__.__name__}] ({time.time()-t0}) état={self.contenu}")
-                print(f"[{self.__class__.__name__}] ({time.time()-t0}) post-it '{postit}' libéré")
-            self.contenu=np.delete(self.contenu,-1)
+                    print(f"[{self.__class__.__name__}] état={self.contenu}")
+                print(f"[{self.__class__.__name__}] post-it '{self.contenu[-1]}' libéré")
+            postit=self.contenu.pop(-1)
             logging.info(f"Le post-it {postit} fut libéré au bout de {time.time()-t0} secondes")
             return postit
 
@@ -47,84 +48,117 @@ class Bar(Accessoire):
     def recevoir(self,plateau):
         self.contenu.append(plateau)
         if verbo!=0:
-            print(f"[{self.__class__.__name__}] ({time.time()-t0}) '{plateau}' reçu")
+            print(f"[{self.__class__.__name__}]   '{plateau}' reçu")
             if verbo==2:
-                print(f"[{self.__class__.__name__}] ({time.time()-t0}) état={self.contenu}")
+                print(f"[{self.__class__.__name__}] état={self.contenu}")
         logging.info(f"Le plateau {plateau} fut reçu par le bar au bout de {time.time()-t0} secondes")
         return self.contenu
     def evacuer(self):
         if len(self.contenu)==0:
-            if verbo==2 : print(f"[{self.__class__.__name__}] ({time.time()-t0}) état={self.contenu}")
+            if verbo==2 : print(f"[{self.__class__.__name__}]   état={self.contenu}")
             logging.info(f"Le bar est vide au bout de {time.time()-t0} secondes")
-            print(f" ({time.time()-t0}) Bar est vide")
+            print(f"Bar est vide")
         else:
-            plateau=self.contenu[-1]
             if verbo!=0:
                 if verbo==2:
-                    print(f"[{self.__class__.__name__}] ({time.time()-t0}) état={self.contenu}")
-                print(f"[{self.__class__.__name__}] ({time.time()-t0}) '{plateau}' évacué")
-            self.contenu=np.delete(self.contenu,-1)
+                    print(f"[{self.__class__.__name__}] état={self.contenu}")
+                print(f"[{self.__class__.__name__}] '{self.contenu[-1]}' évacué")
+            plateau=self.contenu.pop(-1)
             logging.info(f"Le plateau {plateau} fut évacué du bar au bout de {time.time()-t0} secondes")
             return plateau
 
-class Serveur:
+class Serveur(threading.Thread):
     def __init__(self,pic,bar,commandes):
+        threading.Thread.__init__(self)
+        self.commandes=commandes
         self.plateau=[]
-        print(f"[{self.__class__.__name__}] ({time.time()-t0}) prêt pour le service!")
+        print(f"[{self.__class__.__name__}]   prêt pour le service!")
         logging.info(f"Le serveur est prêt au bout de {time.time()-t0} secondes")
-    def prendre_commande(self):
+    async def prendre_commande(self):
         """ Prend une commande et embroche un post-it. """
-        for i in commandes:
-            print(f"[{self.__class__.__name__}] ({time.time()-t0}) je prends commande de '{i}'" )
-            logging.info(f"La commande {i} est prise par le serveur au bout de {time.time()-t0} secondes")
-            pic.embrocher(i)
+        while commandes!=[]:
+            com=commandes.pop(-1)
+            print(f"[{self.__class__.__name__}]   je prends commande de '{com}'" )
+            logging.info(f"La commande {com} est prise par le serveur au bout de {time.time()-t0} secondes")
+            pic.embrocher(com)
+            await asyncio.sleep(2)
         if verbo!=0:
             if verbo==2:
-               print(f"[{self.__class__.__name__}] ({time.time()-t0}) il n'y a plus de commande à prendre ")
+               print(f"[{self.__class__.__name__}] il n'y a plus de commande à prendre ")
             logging.info(f"Le serveur n'a plus de commandes à prendre au bout de {time.time()-t0} secondes")
-            print(f" ({time.time()-t0}) plus de commande à prendre")
-    def servir(self):
+            print(f"plus de commande à prendre")
+    async def servir(self):
         """ Prend un plateau sur le bar. """
-        for i in bar.contenu:
+        while commandes!=[] or pic.contenu!=[] or bar.contenu!=[]:
+            if bar.contenu==[]:
+                await asyncio.sleep(5)
             com=bar.evacuer()
-            logging.info(f"La commande {i} fut servie par le serveur au bout de {time.time()-t0} secondes")
-            print(f"[{self.__class__.__name__}] ({time.time()-t0}) je sers '{com}'")
+            logging.info(f"La commande {com} fut servie par le serveur au bout de {time.time()-t0} secondes")
+            print(f"[{self.__class__.__name__}]  je sers '{com}'")
+            await asyncio.sleep(5)
         if verbo!=0: bar.evacuer()
+    async def s(self):
+        print('hey')
+        async with asyncio.TaskGroup() as tg:
+            task1 = tg.create_task(self.prendre_commande())
+            task2 = tg.create_task(self.servir())
+    def run(self):
+        asyncio.run(self.s())
 
-class Barman:
+class Barman(threading.Thread):
     def __init__(self,pic,bar):
+        threading.Thread.__init__(self)
         self.pic=[]
         self.bar=[]
+        self.actif=False
         logging.info(f"Le barman est prêt au bout de {time.time()-t0} secondes")
         print(f"[{self.__class__.__name__}] prêt pour le service!")
-    def preparer(self):
+    async def nettoyer(self):
+        while commandes!=[] or pic.contenu!=[]:
+            await asyncio.sleep(1.5)
+            print(f"[{self.__class__.__name__}] Je nettoie le comptoir")
+    async def preparer(self):
         """ Prend un post-it, prépare la commande et la dépose sur le bar. """
-        for i in pic.contenu:
+        while commandes!=[] or pic.contenu!=[]:
             com=pic.liberer()
-            logging.info(f"La comande {com} est initiée au bout de {time.time()-t0} secondes")
-            print(f"[{self.__class__.__name__}] ({time.time()-t0}) je commence la fabrication de '{com}'")
-            time.sleep(0.4)
-            logging.info(f"La comande {com} est finie au bout de {time.time()-t0} secondes")
-            print(f"[{self.__class__.__name__}] ({time.time()-t0}) je termine la fabrication de '{com}'")
+            logging.info(f"La commande {com} est initiée au bout de {time.time()-t0} secondes")
+            print(f"[{self.__class__.__name__}] je commence la fabrication de '{com}'")
+            await asyncio.sleep(4)
+            logging.info(f"La commande {com} est finie au bout de {time.time()-t0} secondes")
+            print(f"[{self.__class__.__name__}] je termine la fabrication de '{com}'")
             bar.recevoir(com)
         if verbo!=0: pic.liberer()
-        
+    async def f(self):
+        async with asyncio.TaskGroup() as tg:
+            task3 = tg.create_task(self.preparer())
+            task4 = tg.create_task(self.nettoyer())
+    def run(self):
+        asyncio.run(self.f())
+
 #le degré de verbosité, s'il est entré, sera le 1er argument (1 ou 2, le niveau 0 est par défaut)
 
-if __name__=="__main__":  
-    t0=time.time()
+
+async def main(): 
+    async with asyncio.TaskGroup() as tg:
+      task1 = tg.create_task(Steve.prendre_commande())
+      task2 = tg.create_task(Steve.servir())
+
+async def second():
+    async with asyncio.TaskGroup() as tg:
+        task3 = tg.create_task(Bob.preparer())
+        task4 = tg.create_task(Bob.nettoyer())
+
+if __name__ == "__main__":
     if sys.argv[1]=='1' or sys.argv[1]=='2':
         verbo=int(sys.argv[1]) 
-        commandes=reversed(sys.argv[2:])
+        commandes=sys.argv[2:]
     else:
         verbo=0
-        commandes=reversed(sys.argv[1:])
+        commandes=sys.argv[1:]
     pic=Pic()
     bar=Bar()
-    print(commandes)
     Bob=Barman(pic,bar)
     Steve=Serveur(pic,bar,commandes)
-    Steve.prendre_commande()
-    Bob.preparer()
-    Steve.servir()
-
+    #asyncio.run(main())
+    Steve.start()
+    Bob.start()
